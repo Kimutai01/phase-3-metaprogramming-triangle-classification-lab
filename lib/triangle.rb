a57c23a2f0df4a6bf9416ef19b3fd889cf5094d1
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-25 18:05:09 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
+
 class Triangle
   # write code here
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @sides.any? {|side| side <= 0} || @sides[0] + @sides[1] <= @sides[2] || @sides[1] + @sides[2] <= @sides[0] || @sides[2] + @sides[0] <= @sides[1]
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
